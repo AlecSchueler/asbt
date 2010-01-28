69a38791cdba7679e79482992a4079930b354efe
AlecSchueler <johannalecschueler@googlemail.com>
2010-01-28 14:05:53 +0000

    drafted setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "asbt",
+    version = "0.9.0",
+    author = "Alec Schueler",
+    author_email = "johannalecschueler@googlemail.com",
+    url = "", #XXX
+    license = "New BSD License",
+    data_files = [("man.1", ["man/asbt.1"])],
+    scripts = "asbt",
+    claassifiers = [
+        "Development Status :: Beta",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Enviroment :: Console",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Bug Tracking",
+        ]
+)
